1c3b108455d927880662220ad03d678bc27a6e28
Mehran Hydary <mehran.hydary@gmail.com>
2020-02-06 11:57:23 -0500

    Creating repo for the visual app...

---
+++ client-visual/src/sections/Investment/Investment.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+import { gql } from 'apollo-boost'
+import { useQuery, useMutation } from 'react-apollo'
+
+interface Props {
+    title: string
+}
+
+export const Investment = () => {
+    
+}
---
+++ client-visual/src/sections/Investment/index.tsx
@@ -0,0 +1,1 @@
+export * from './Investment'
