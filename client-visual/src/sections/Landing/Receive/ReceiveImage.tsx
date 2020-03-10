1a07284c62964d95ac6fdb0d877fbe20b670d355
Mehran Hydary <mehran.hydary@gmail.com>
2020-03-10 09:49:11 -0400

    updated image names in the landing page files

--- client-visual/src/sections/Landing/Receive/ReceiveImage.tsx
+++ client-visual/src/sections/Landing/Receive/ReceiveImage.tsx
@@ -12,7 +12,7 @@ export const ReceiveImage = () => {
     const classes = useStyles()
     return (
             <Grid item xs={6} sm={6} md={3}>
-              <img className={classes.imgSpecs} src='./Infographic:receive:mobile.png' alt='ratio of crypto pie chart' />
+              <img className={classes.imgSpecs} src='./Infographic_receive_mobile.png' alt='ratio of crypto pie chart' />
             </Grid>
     )
 }
