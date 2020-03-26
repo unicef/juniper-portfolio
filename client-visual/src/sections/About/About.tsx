fd9fb82aff79b72c2ffed706facf16e84d84cc5a
Elisa Pasquali <epasquali@hotmail.com>
2020-03-26 17:52:27 +0100

    Finish about page

--- client-visual/src/sections/About/About.tsx
+++ client-visual/src/sections/About/About.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { Grid } from '@material-ui/core'
 import { makeStyles} from '@material-ui/styles'
 import { AboutMainText } from './AboutMainText'
+import {ApplicationImg, ApplicationMiddleImg} from './AboutImgs'
 
 const useStyles = makeStyles((theme: any) => ({
-    
 
     top: {
         backgroundColor: '#ffd113',
@@ -77,9 +77,21 @@ export const About = () => {
                     
                     <Grid item sm={6} className={classes.rcolumn}>
                         <AboutBlurb1 />
+                        <ApplicationImg />
                         <AboutBlurb2 />
                         <AboutBlurb3 />
                         <AboutBlurb4 />
+                    </Grid>
+                </Grid>
+                <ApplicationMiddleImg />
+                <Grid container>
+                    <Grid item sm={6} className={classes.lcolumn}>
+                        <div className={classes.largetext}>Why blockchain?</div>
+                    </Grid>
+                    
+                    <Grid item sm={6} className={classes.rcolumn}>
+                        <AboutBlurb5 />
+                        <AboutBlurb6 />
                     </Grid>
                 </Grid>
 
@@ -156,6 +168,41 @@ export const AboutBlurb4 = () => {
                     another developer or two are needed, design help to communicate what the project
                     can do, or some server space. Refactoring something into a new language because 
                     the original one won't scale. Testing it in a new area. Getting some data points.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+
+export const AboutBlurb5 = () => {
+    const classes = useStyles();
+    
+    return (
+        <div>
+            <div className={classes.header}>Enabling a digitally financed future</div>
+            <div className={classes.blurb}>
+                <p>By distributing funding in cryptocurrency, UNICEF, donors, recipients,
+                    and the public can track where the money is going and how it is being spent,
+                    providing an unprecedented level of transparency in the funding and NGO space.
+                    This is due to blockchain technology, the tool powering cryptocurrency.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+
+export const AboutBlurb6 = () => {
+    const classes = useStyles();
+    
+    return (
+        <div>
+            <div className={classes.header}>A permanent, transparent record of a financial transaction</div>
+            <div className={classes.blurb}>
+                <p>Blockkchain technology operates as a digital ledger, recording each transaction which
+                    takes place on the network. With public cryptocurrencies such as bitcoin and ether, anyone is
+                    able to see where and when the assets are moving, revolutionizing the way giving today works.
                 </p>
             </div>
         </div>
