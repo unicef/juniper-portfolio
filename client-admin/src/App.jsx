225e8474271c52b594160aac198474e0c41f62ec
tenthirtyone <alexsherbuck@gmail.com>
2020-04-30 14:32:03 -0400

    Top-level class component

--- client-admin/src/App.jsx
+++ client-admin/src/App.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-export default class App {
+export default class App extends Component {
   render() {
     return (
       <div className="App">
