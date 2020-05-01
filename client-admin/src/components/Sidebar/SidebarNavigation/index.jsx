6a7741d37f01c32e20a27fcaaf74f38c35fb4dc2
tenthirtyone <alexsherbuck@gmail.com>
2020-05-01 14:10:27 -0400

    sidebar styling

--- client-admin/src/components/Sidebar/SidebarNavigation/index.jsx
+++ client-admin/src/components/Sidebar/SidebarNavigation/index.jsx
@@ -1,23 +1,16 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
-import FormGroup from '@material-ui/core/FormGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import FolderIcon from '@material-ui/icons/Folder';
-import DeleteIcon from '@material-ui/icons/Delete';
+import InboxIcon from '@material-ui/icons/Inbox';
 
 const useStyles = makeStyles((theme) => ({
-    navIcon: {},
+    navIcon: {
+        color: "#ffffff",
+    },
     navText: {
         fontSize: 12,
         fontWeight: 700,
@@ -28,30 +21,63 @@ const useStyles = makeStyles((theme) => ({
         textAlign: 'left',
         color: '#ffffff',
         textTransform: 'uppercase',
-
-    }
+    },
+    navLink: {
+        height: 55,
+        paddingLeft: 20
+    },
 }));
 
+const JuniperListItem = withStyles({
+    root: {
+        "&$selected": {
+            backgroundColor: "#002452",
+            "&:hover": {
+                backgroundColor: "#002452"
+            }
+        },
+        "&:hover": {
+            backgroundColor: "rgba(0,0,0,.14)"
+        }
 
-export default function SidebarNavigation() {
+    },
+    selected: {}
+})(ListItem);
+
+export default function SelectedListItem() {
     const classes = useStyles();
+    const [selectedIndex, setSelectedIndex] = React.useState(0);
+
+    const handleListItemClick = (event, index) => {
+        setSelectedIndex(index);
+    };
 
     return (
-        <Grid container spacing={2}>
-            <Grid item xs={12}>
-                <List>
-                    <ListItem>
-                        <ListItemAvatar>
-                            <Avatar>
-                                <FolderIcon />
-                            </Avatar>
-                        </ListItemAvatar>
-                        <ListItemText
-                            primary={<Typography className={classes.navText}>Link Text</Typography>}
-                        />
-                    </ListItem>
-                </List>
-            </Grid>
-        </Grid>
+        <List component="nav" aria-label="Sidebar Navigation">
+            {
+                [0, 1, 2, 3].map((navLink, index) => {
+                    return (
+                        <JuniperListItem
+                            button
+                            selected={selectedIndex === index}
+                            onClick={(event) => handleListItemClick(event, index)}
+                            className={classes.navLink}
+                        >
+                            <ListItemIcon>
+                                <InboxIcon className={classes.navIcon} fontSize="large" />
+                            </ListItemIcon>
+                            <ListItemText
+                                primary={<Typography className={classes.navText}>Link Text</Typography>}
+                            />
+                        </JuniperListItem>
+                    )
+                })
+            }
+
+        </List >
     );
 }
+
+
+
+
