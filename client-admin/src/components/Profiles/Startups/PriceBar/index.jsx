3d86da0d7c29e6878d2c3538b8b45b659bcbe24c
Elisa Pasquali <epasquali@hotmail.com>
2020-05-20 23:30:45 +0200

    Added blue price calculation bar at top of Accounts->Startups page

---
+++ client-admin/src/components/Profiles/Startups/PriceBar/index.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    width: '903px',
+    height: '77px',
+    borderRadius: '5px',
+    borderColor: '#daf5ff',
+    backgroundColor: '#daf5ff',
+
+    color: '#000000',
+    padding: '5px',
+    
+    
+    },
+    
+  
+  cardcontent:
+  {
+      paddingRight: '10px',
+      paddingLeft: '10px',
+   },
+  
+
+  text:
+  {
+    fontSize: '19px',
+    lineHeight: '27px',
+    letterSpacing: 0,      
+  },
+  
+  textbold: 
+  {
+      fontWeight: 700,
+  },
+  
+  button: {
+    paddingLeft: '90px',
+  },
+});
+
+export default function PriceBar() {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root} variant="outlined">
+      <CardContent className={classes.cardcontent}>
+        <Typography className={classes.text} variant="body1" component="span">
+          <span className={classes.textbold}>USD price </span>
+          = Average price accross three cryptoexchanges, calculated at 12:01 p.m. (EST)
+        </Typography>
+              
+        <Button className={classes.button} size="small" color="primary">MORE INFO</Button>
+      </CardContent>
+      
+    </Card>
+  );
+}
--- client-admin/src/components/Profiles/Startups/index.jsx
+++ client-admin/src/components/Profiles/Startups/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import MainContentContainer from '../../../ui/MainContentContainer'
+import PriceBar from './PriceBar'
 
 export default class extends React.Component {
     render() {
         return (
-            <MainContentContainer>Startups</MainContentContainer>
+            <MainContentContainer>
+                <PriceBar/>
+            </MainContentContainer>
         )
     }
 }
