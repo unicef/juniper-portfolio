4d68ed9f4386fac8b3eeafa3aa01c39048719e7a
tenthirtyone <alexsherbuck@gmail.com>
2020-05-21 10:07:22 -0400

    update app nomenclature, function names, class names

---
+++ client-admin/src/components/Accounts/Donors/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import MainContentContainer from '../../../ui/MainContentContainer'
+
+export default function Donors() {
+    return (
+        <MainContentContainer>Donors</MainContentContainer>
+    )
+}
