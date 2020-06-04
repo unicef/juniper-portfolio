7241a249c92a970df35ec227b662731a4b4ebd6f
Elisa Pasquali <epasquali@hotmail.com>
2020-06-04 17:23:02 +0200

    Added back the margin.

--- client-admin/src/ui/MainContentContainer.jsx
+++ client-admin/src/ui/MainContentContainer.jsx
@@ -3,7 +3,8 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        padding: "46px 50px 70px 155px",
+        marginTop: "5em",
+        padding: "0px 50px 70px 155px",
         backgroundColor: '#f8f8f8',
     }
 }));
