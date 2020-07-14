da342cff47790ca0a5217901674a0183cf9696f9
Elisa Pasquali <epasquali@hotmail.com>
2020-07-14 17:55:43 +0200

    Card formatting on bitcoin price tracker page

--- client-admin/src/components/PriceTracker/Bitcoin/index.jsx
+++ client-admin/src/components/PriceTracker/Bitcoin/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
 import MainContentContainer from '../../../ui/MainContentContainer'
 import PriceBar from '../../PriceBar'
 
@@ -21,7 +22,7 @@ const useStyles = makeStyles({
     lineHeight: 1.17,
     letterSpacing: 'normal',
     color: '#000000',
-    paddingTop: '40px',
+    padding: '40px 0px 10px 0px',
   },
 
   subheader:
@@ -34,15 +35,76 @@ const useStyles = makeStyles({
 
   },
   
+  card:
+  {
+    paddingTop: '40px',
+  },
+
+  cardinner:
+  {
+      padding: '40px',
+  },
+  
+  cardheader:
+  {
+    fontFamily: '"Roboto", sans-serif',
+    fontSize: '28px',
+    lineHeight: '28px',
+    fontWeight: 'bold',
+    color: '#000000',
+    paddingBottom:'30px',
+
+  },
+
+  cardsubheader:
+  {
+    fontFamily: '"Roboto", sans-serif',
+    fontSize: '28px',
+    lineHeight: '28px',
+    fontWeight: 'normal',
+    color: '#000000',
+    paddingBottom: '30px',
+  },
+
+  smalltext:
+  {
+    fontFamily: '"Cabin", sans-serif',
+    fontSize: '10px',
+    letterSpacing: '0.83px',
+    paddingBottom: '5px',
+
+  },
+
 });
 
 export default function BitcoinPriceTracker() {
   const classes = useStyles();
+  const date = new Date();
+  const month = date.toLocaleString('default', { month: 'short' });
+  const datestring = date.getUTCDate() + " " + month + " " + date.getUTCFullYear();
+  
   return (
     <MainContentContainer className={classes.root}>
       <PriceBar />
       <div className={classes.header}>Bitcoin price overview</div>
-      <div className={classes.subheader}>Date goes here</div>
+      <div className={classes.subheader}>{datestring}</div>
+      <MainCard />
     </MainContentContainer>
         )
+}
+
+export function MainCard() {
+  const classes = useStyles();
+  return (
+    <div className={classes.card}>
+      <Card className={classes.cardinner} variant="outlined">
+        <div className={classes.smalltext}>TODAY'S AVERAGE PRICE</div>
+        <div className={classes.cardheader}>8798 USD</div>
+        <div className={classes.smalltext}>MONTHLY AVERAGE PRICE</div>
+        <div className={classes.cardsubheader}>8798 USD</div>
+        <div className={classes.smalltext}>Q1 AVERAGE PRICE</div>
+        <div className={classes.cardsubheader}>8798 USD</div>
+      </Card>
+    </div>
+  )
 }
