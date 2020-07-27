26d49e69bc52cb34c9544555941a51425d7c6ded
tenthirtyone <alexsherbuck@gmail.com>
2020-07-27 16:13:36 -0400

    dell touchpads have cost me more lost productivity than my cat that bothers me 3-5 times every hour.

--- client-admin/src/components/Settings/UserManagement/ExistingUsers.jsx
+++ client-admin/src/components/Settings/UserManagement/ExistingUsers.jsx
@@ -1,85 +1,126 @@
-import React, { useState } from "react";
+import React, { Fragment } from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Divider from "@material-ui/core/Divider";
 import Grid from "@material-ui/core/Grid";
-import Select from "@material-ui/core/Select";
-import MenuItem from "@material-ui/core/MenuItem";
+import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
+import GenerateLinkIcon from "../../../ui/Icons/GenerateLinkIcon";
+import EnvelopeIcon from "../../../ui/Icons/EnvelopeIcon";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
-    backgroundColor: "#ffffff",
+    width: "100%",
   },
-  select: {
-    fontFamily: '"Roboto", sans-serif',
-    paddingLeft: 10,
-    fontSize: 16,
-    lineHeight: 1.69,
-    width: 450,
-    paddingBottom: 8,
-    "& .MuiSelect-root:focus": {
-      backgroundColor: "#ffffff",
-    },
-  },
-  menuitem: {
+  avatar: {
     fontFamily: '"Roboto", sans-serif',
     fontSize: 16,
-    "&:hover": {
-      backgroundColor: "#daf5ff",
-    },
+    fontWeight: 700,
+    lineHeight: 1.75,
+    height: 47,
+    width: 47,
+    color: "#374ea2",
+    backgroundColor: "#daf5ff",
   },
-  filledButton: {
-    width: 202,
-    height: 35,
-    fontFamily: '"Cabin", sans-serif',
+  listItem: {
+    fontFamily: '"Roboto", sans-serif',
+    fontSize: 16,
+    lineHeight: 1.6,
+    marginTop: 12,
+    marginBottom: 12,
+    padding: 15,
+  },
+  username: {
+    fontFamily: '"Roboto", sans-serif',
+    fontSize: 16,
+    fontWeight: 700,
+    lineHeight: 1.5,
+    marginTop: 0,
+    marginBottom: 0,
+  },
+  position: {
+    fontFamily: '"Roboto", sans-serif',
+    fontSize: 14,
+    lineHeight: 1.64,
+    color: "#898989",
+    marginTop: 0,
+    marginBottom: 0,
+  },
+  joinMessage: {
+    fontFamily: '"Roboto", sans-serif',
+    fontSize: 14,
+    lineHeight: 1.64,
+    color: "#898989",
+    marginTop: 0,
+    marginBottom: 0,
+  },
+  generatLinkButton: {
+    marginTop: "1em",
+    float: "left",
     fontSize: 12,
     fontWeight: 700,
-    textAlign: "center",
-    color: "#ffffff",
-    boxShadow: "none",
-    marginTop: 35,
+    letterSpacing: 1,
+    fontFamily: '"Cabin", sans-serif',
+    color: "#00aeef",
+    "&:hover": {
+      backgroundColor: "#ecfaff",
+    },
+    "& .MuiButton-endIcon": {
+      margin: 0,
+    },
+    paddingLeft: 0,
+    "& .MuiButton-startIcon": {
+      margin: 0,
+      marginTop: 8,
+    },
   },
-});
+}));
 
-export default function AddNewUser() {
+export default function ActivityList(props) {
   const classes = useStyles();
-  const [workUnit, setWorkUnit] = useState("");
 
   return (
-    <Grid container className={classes.root}>
-      <Grid item xs={12}>
-        <Select
-          value={workUnit}
-          onChange={(e) => {
-            setWorkUnit(e.target.value);
-          }}
-          className={classes.select}
-        >
-          <MenuItem className={classes.menuitem} value={"DFAM"}>
-            DFAM
-          </MenuItem>
-          <MenuItem className={classes.menuitem} value={"Office of Innovation"}>
-            Office of Innovation
-          </MenuItem>
-          <MenuItem className={classes.menuitem} value={"ICTD"}>
-            ICTD
-          </MenuItem>
-          <MenuItem className={classes.menuitem} value={"Operations (OOI)"}>
-            Operations (OOI)
-          </MenuItem>
-          <MenuItem className={classes.menuitem} value={"WASH"}>
-            WASH
-          </MenuItem>
-        </Select>
-      </Grid>
-      <Grid item xs={12}>
-        <Button
-          className={classes.filledButton}
-          variant="contained"
-          color="primary"
-        >
-          Save Changes
-        </Button>
-      </Grid>
-    </Grid>
+    <List component="nav" className={classes.root}>
+      <Fragment>
+        <Divider />
+        <ListItem className={classes.listItem}>
+          <Grid container>
+            <Grid item xs={1}>
+              <Avatar src={null} className={classes.avatar}>
+                MH
+              </Avatar>
+            </Grid>
+            <Grid item xs={4}>
+              <p className={classes.username}>Christopher Waltz</p>
+              <p className={classes.position}>Director, DFAM</p>
+            </Grid>
+            <Grid item xs={4}>
+              <p className={classes.joinMessage}>User joined on June 6 2020</p>
+            </Grid>
+            <Grid item xs={3}>
+              <Button
+                className={classes.generatLinkButton}
+                startIcon={<EnvelopeIcon />}
+                onClick={async () => {
+                  console.log("button");
+                }}
+              >
+                Send New Invite
+              </Button>
+              <Button
+                className={classes.generatLinkButton}
+                startIcon={<GenerateLinkIcon />}
+                onClick={async () => {
+                  console.log("button");
+                }}
+              >
+                Generate Invite Link
+              </Button>
+            </Grid>
+          </Grid>
+        </ListItem>
+      </Fragment>
+    </List>
   );
 }
