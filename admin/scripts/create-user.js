6136eef2a7f238be70edd96add04136cb2cb35ee
tenthirtyone <alexsherbuck@gmail.com>
2020-07-29 11:20:44 -0400

    clean up

--- admin/scripts/create-user.js
+++ admin/scripts/create-user.js
@@ -3,15 +3,13 @@ const config = require("../config");
 
 const juniper = new Juniper(config);
 
-console.log(juniper);
-
 const user = {
   firstName: "Admin",
   lastName: "User",
   email: "admin@user.com",
   picture: "url",
   title: "Admin",
-  department: "UNICEF",
+  department: "Office of Innovation",
   password: "asdf1234",
   salt: "",
   notifications: true,
