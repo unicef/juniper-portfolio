809e5d57218b13c8cc2a4aefcc45854f45db94f8
tenthirtyone <alexsherbuck@gmail.com>
2020-09-21 13:36:52 -0400

    published tx card buttons

--- client-admin/src/ui/Cards/ArchivedTransactionCard.jsx
+++ client-admin/src/ui/Cards/ArchivedTransactionCard.jsx
@@ -1,7 +1,6 @@
 import React, { Fragment } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Divider from "@material-ui/core/Divider";
-import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 import TxSentIcon from "../Icons/TxReceivedIcon";
