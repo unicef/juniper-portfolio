670786fb8c39cd3593e467233d57bd565efad495
tenthirtyone <alexsherbuck@gmail.com>
2020-09-21 15:03:48 -0400

    copy button snackbar

--- client-admin/src/ui/Buttons/CopyAddressButton.jsx
+++ client-admin/src/ui/Buttons/CopyAddressButton.jsx
@@ -1,18 +1,42 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
 import TextButton from "./TextButton";
 import CopyIcon from "../Icons/CopyIcon";
 import { copyToClipboard } from "../../actions";
+import Snackbar from "../Snackbar";
 
 export default function (props) {
+  const [showSnackbar, setShowSnackbar] = useState(false);
+  const [snackbarDuration] = useState(3000);
+  const [snackbarMessage, setSnackbarMessage] = useState(
+    `Address copied to clipboard`
+  );
+  const [snackbarSeverity, setSnackbarSeverity] = useState("success");
   return (
-    <TextButton
-      startIcon={<CopyIcon style={{ fontSize: 18 }} />}
-      onClick={() => {
-        copyToClipboard(props.address);
-      }}
-      {...props}
-    >
-      {props.children ? props.children : "Copy Address"}
-    </TextButton>
+    <Fragment>
+      <Snackbar
+        open={showSnackbar}
+        severity={snackbarSeverity}
+        duration={snackbarDuration}
+        message={snackbarMessage}
+        onClose={() => {
+          setShowSnackbar(false);
+        }}
+        icon={<CopyIcon style={{ stroke: "#ffffff", fill: "rgba(0,0,0,0)" }} />}
+      />
+      <TextButton
+        startIcon={
+          <CopyIcon
+            style={{ fontSize: 18, stroke: "#00AEEF", fill: "rgba(0,0,0,0)" }}
+          />
+        }
+        onClick={() => {
+          copyToClipboard(props.address);
+          setShowSnackbar(true);
+        }}
+        {...props}
+      >
+        {props.children ? props.children : "Copy Address"}
+      </TextButton>
+    </Fragment>
   );
 }
